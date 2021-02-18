ea3e149fe940f76178ba4df60ac3bc4d40241692
John Higuita <johnfrehiguita@gmail.com>
2021-02-18 17:10:05 -0500

    enable api

--- InmueblesPeru/urls.py
+++ InmueblesPeru/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from api import views as api_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
