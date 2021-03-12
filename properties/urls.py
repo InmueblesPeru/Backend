18f3838c60a15173ddd30870fc4282f6bac253e9
John Higuita <johnfrehiguita@gmail.com>
2021-03-12 01:31:43 -0500

    set up endpoint propierties

--- properties/urls.py
+++ properties/urls.py
@@ -3,8 +3,15 @@
 # Django
 from django.urls import path
 
+# Restframework
+# from rest_framework.routers import DefaultRouter
+
 # Views
 from properties.views import ListProperties
+
+#router = DefaultRouter()
+#router.register(r'properties', ListProperties, basename='property')
+#urlpatterns = router.urls
 
 urlpatterns = [
     path('properties/', ListProperties.as_view()),
--- properties/views/properties.py
+++ properties/views/properties.py
@@ -1,3 +1,7 @@
+# Django
+
+from django.http import Http404
+
 # Rest_framework
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -12,9 +16,17 @@ from properties.serializers.properties import PropertyModelSerializer
 class ListProperties(APIView):
     """View to list all properties in the system"""
 
-    def get(self, request, format=None):
+    def get(self, request):
         """Return a list of all properties"""
-        properties = Property.objects.all()
-        serializer = PropertyModelSerializer(properties, many=True)
+
+        if request.data.get('id') != None:
+            try:
+                properties = Property.objects.get(pk=self.request.data.get('id'))
+            except Property.DoesNotExist:
+                raise Http404
+            serializer = PropertyModelSerializer(properties)
+        else:
+            properties = Property.objects.all()
+            serializer = PropertyModelSerializer(properties, many=True)
 
         return Response(serializer.data)
